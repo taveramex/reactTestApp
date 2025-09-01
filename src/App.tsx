e07b18c9326af4de6e642a06609cd00b910767cc
Armando Tavera <taveramex@gmail.com>
2025-09-01 16:55:43 -0600

    Agregando estilos Kawaii a toda la App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import MyAwesomeApp from "./components/MyAwesomeApp";
 
 export function App() {
     return (
-        <>
+        <div className="app-container">
             <h1>Hello there sweetie</h1>
             <p>
                 Too much cutie on your beauty
             </p>
             <button>looking very nice</button>
             <MyAwesomeApp />
-        </>
+        </div>
     );
 }
