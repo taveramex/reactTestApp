fe2da679a54a75cfef943d04b6562943fdd932b2
Armando Tavera <taveramex@gmail.com>
2025-09-03 18:36:45 -0600

    implementando groserias fuertes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import MyAwesomeApp from "./components/MyAwesomeApp";
+import memeImg from "./assets/groseriasFuertes.png"; // Importa la imagen
+
 
 export function App() {
     return (
         <div className="app-container">
-            <h1>Hello there sweetie</h1>
+            <h1>Hello Tics</h1>
             <p>
-                Too much cutie on your beauty
+                Groserias fuertes para Tico
             </p>
-            <button>looking very nice</button>
+            <img
+                src={memeImg} // Usa la variable importada
+                alt="Meme"
+                style={{
+                    display: "block",
+                    margin: "24px auto",
+                    maxWidth: "320px",
+                    borderRadius: "12px",
+                    boxShadow: "0 4px 24px #000a"
+                }}
+            />
+            <button>deploy biolencia</button>
             <MyAwesomeApp />
         </div>
     );
